fd438b31210dbf8f077d9614fa4c6b638d8d5a73
uriface6 <80878614+uriface6@users.noreply.github.com>
2021-04-01 18:37:12 +0300

    start work with sequenceVector

--- Shumzy_And_Leib_Symon/Main.cpp
+++ Shumzy_And_Leib_Symon/Main.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <time.h>
 
 #define UP 0
 #define RIGHT 1
 #define DOWN 2
 #define LEFT 3
 
+#define UP_ASCII 72
+#define RIGHT_ASCII 77
+#define DOWN_ASCII 80
+#define LEFT_ASCII 75
+
 using namespace std;
 
 void Game();
+void getUserInput(char*, bool*);
+void addNextMove(vector<int>*);
 void printArrow(const int);
-
+void printMyVector(const vector<int>);
 
 int main()
 {
     system("color B");
 
-    cout << "Hello Symon" << endl << endl;
-    printArrow(UP);
-    printArrow(RIGHT);
-    printArrow(DOWN);
-    printArrow(LEFT);
-
     try
     {
         Game();
     }
-    catch (const std::exception&)
+    catch (const exception&)
     {
-
+        cout << "Exeption EROR!!!!!!!!!!!!!!" << endl;
     }
 
     system("pause");
@@ -36,23 +39,71 @@ int main()
 
 void Game()
 {
-    char flag = ' ';
+    char userInput = ' ';
+    bool firstFlag = true;
+    vector<int> symonSequence = {};
 
     cout << "Welcome to Shumzy&Leib Symon!!!!" << endl;
+    cout << R"(To play enter any key, to exit (any time) enter 'x':)" << endl;
 
-    while (flag != 'x')
+    while (userInput != 'x')
     {
+        getUserInput(&userInput, &firstFlag);
+        addNextMove(&symonSequence);
         
-        cout << R"(To play enter any key, to exit (any time) enter 'x':)" << endl;
-        flag = _getwch();
-        cout << "your flag is: " << flag << endl;
-        cout << "your flag is int : " << (int)flag << endl;
-        if ((int)flag == -32)
-        {
-            flag = _getwch();
-            
-        }
     }
+    printMyVector(symonSequence);
+}
+
+void getUserInput(char* userInput, bool* firstFlag)
+{
+    *userInput = _getwch();
+        //cout << "your userInput is: " << userInput << endl;
+        //cout << "your userInput is int : " << (int)userInput << endl;
+        if ((int)*userInput == -32)
+        {
+            //cout << "Yoe Pressesd on Arrow!!!" << endl;
+            *userInput = _getwch();
+            //cout << "your Arrow is: " << (int)userInput << endl;
+            
+            switch (*userInput)
+            {
+            case UP_ASCII:
+                printArrow(UP);
+                break;
+
+            case RIGHT_ASCII:
+                printArrow(RIGHT);
+                break;
+
+            case DOWN_ASCII:
+                printArrow(DOWN);
+                break;
+
+            case LEFT_ASCII:
+                printArrow(LEFT);
+                break;
+            default:
+                break;
+            }
+
+            *firstFlag = false;
+        }
+        else if(*userInput == 'x' || *firstFlag)
+        {
+            *firstFlag = false;
+        }
+        else
+        {
+            cout << R"(Press only on tha Arrows or 'x')" << endl;
+        }
+}
+
+void addNextMove(vector<int>* symonSequence)
+{
+    int nextMove = rand() % 4;
+    cout << "your next move is:" << nextMove << endl;
+    symonSequence->push_back(nextMove);
 }
 
 void printArrow(const int arrow)
@@ -93,7 +144,18 @@ void printArrow(const int arrow)
         break;
 
     default:
+        cout << "ERORR!!!!!!!!!!!!!!!" << endl; 
         break;
     }
+}
+
+void printMyVector(vector<int> myVector)
+{
+    cout << "your vector is: ";
+    for (vector<int>::iterator it = myVector.begin(); it != myVector.end(); ++it)
+    {
+        cout << *it << ", ";
+    }
+    cout << endl << endl;
 }
 
