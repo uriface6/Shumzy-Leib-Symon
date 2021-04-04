77abc72f3fcb48c36b7b9f718d9fb27ca0aa6ee5
uriface6 <80878614+uriface6@users.noreply.github.com>
2021-04-04 17:02:59 +0300

    end basics symon with arrows
    the basic code for the symon with arrows

--- Shumzy_And_Leib_Symon/Main.cpp
+++ Shumzy_And_Leib_Symon/Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <time.h>
+#include<windows.h>
+
 
 #define UP 0
 #define RIGHT 1
@@ -15,8 +17,11 @@
 using namespace std;
 
 void Game();
-void getUserInput(char*, bool*);
+void startNewGame(char*);
+void getUserInput(char*);
 void addNextMove(vector<int>*);
+void move(char*, vector<int>*);
+int arrowToInt(const char);
 void printArrow(const int);
 void printMyVector(const vector<int>);
 
@@ -40,23 +45,36 @@ int main()
 void Game()
 {
     char userInput = ' ';
-    bool firstFlag = true;
     vector<int> symonSequence = {};
 
     cout << "Welcome to Shumzy&Leib Symon!!!!" << endl;
-    cout << R"(To play enter any key, to exit (any time) enter 'x':)" << endl;
+    //cout << R"(To play enter any key, to exit (any time) enter 'x':)" << endl;
 
     while (userInput != 'x')
     {
-        getUserInput(&userInput, &firstFlag);
-        addNextMove(&symonSequence);
-        
+        //getUserInput(&userInput, &firstFlag);
+        //addNextMove(&symonSequence);
+        startNewGame(&userInput);
+        if (userInput == 'x')
+        {
+            break;
+        }
+        move(&userInput, &symonSequence);
     }
-    printMyVector(symonSequence);
+    //printMyVector(symonSequence);
 }
 
-void getUserInput(char* userInput, bool* firstFlag)
+void startNewGame(char* userInput)
 {
+    cout << "To start new game anter any key, to exit enter x (any time): " << endl;
+    *userInput = _getwch();
+}
+
+void getUserInput(char* userInput)
+{
+    system("CLS");
+    system("color B");
+
     *userInput = _getwch();
         //cout << "your userInput is: " << userInput << endl;
         //cout << "your userInput is int : " << (int)userInput << endl;
@@ -86,28 +104,88 @@ void getUserInput(char* userInput, bool* firstFlag)
             default:
                 break;
             }
-
-            *firstFlag = false;
-        }
-        else if(*userInput == 'x' || *firstFlag)
-        {
-            *firstFlag = false;
         }
         else
         {
-            cout << R"(Press only on tha Arrows or 'x')" << endl;
+            cout << R"(Press only on tha Arrows)" << endl;
         }
 }
 
 void addNextMove(vector<int>* symonSequence)
 {
     int nextMove = rand() % 4;
-    cout << "your next move is:" << nextMove << endl;
+    //cout << "your next move is:" << nextMove << endl;
     symonSequence->push_back(nextMove);
+}
+
+void move(char* userInput, vector<int>* symonSequence)
+{
+    bool endGame = false;
+    int arrowInt = -1;
+
+    addNextMove(symonSequence);
+
+    while (!endGame)
+    {
+        printMyVector(*symonSequence);
+        ///cout << "User:::::::::::" << endl;
+        for (vector<int>::iterator it = symonSequence->begin(); it != symonSequence->end() && !endGame; ++it)
+        {
+            getUserInput(userInput);
+            arrowInt = arrowToInt(*userInput);
+            if (!(arrowInt == *it))
+            {
+                endGame = true;
+                cout << "You Lose!!!!!!!!!" << endl;
+                symonSequence->clear();
+                break;
+            }
+
+
+        }
+        if (!endGame) 
+        { 
+            addNextMove(symonSequence); 
+        }
+        system("CLS");
+        Sleep(800);
+
+    }
+}
+
+int arrowToInt(const char arrow)
+{
+    int returnInt = -1;
+
+    switch (arrow)
+    {
+    case UP_ASCII:
+        returnInt = UP;
+        break;
+
+    case RIGHT_ASCII:
+        returnInt = RIGHT;
+        break;
+
+    case DOWN_ASCII:
+        returnInt = DOWN;
+        break;
+
+    case LEFT_ASCII:
+        returnInt = LEFT;
+        break;
+
+    default:
+        cout << "ERORR!!!" << endl;
+        break;
+    }
+
+    return returnInt;
 }
 
 void printArrow(const int arrow)
 {
+    system("CLS");
     switch (arrow)
     {
     case UP:
@@ -151,11 +229,16 @@ void printArrow(const int arrow)
 
 void printMyVector(vector<int> myVector)
 {
-    cout << "your vector is: ";
+    system("CLS");
+    system("color D");
+
+    //cout << "Computer::::::::::::::::::::::::::::::::::" << endl;
     for (vector<int>::iterator it = myVector.begin(); it != myVector.end(); ++it)
     {
-        cout << *it << ", ";
+        //cout << *it << ", ";
+       printArrow(*it);
+       Sleep(500);
+       system("CLS");
     }
-    cout << endl << endl;
 }
 
