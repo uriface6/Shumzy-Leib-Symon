89cb79aac97eb65afd9dedaec7af29a5531947e0
shumzi ima-pc <ariegg210@gmail.com>
2021-05-16 16:54:52 +0300

    initial commit of rpi-code

---
+++ rpi_code/other/constants.py
@@ -0,0 +1,16 @@
+TIME_TO_SHINE = 0.5
+INTERMEDIATE_DELAY = 0.1
+
+led_red = 17
+led_grn = 13
+led_ylw = 19
+led_blu = 26
+
+btn_red = 12
+btn_grn = 16
+btn_ylw = 20
+btn_blu = 21
+
+led_pins = [led_red, led_grn, led_ylw, led_blu]
+btn_pins = [btn_red, btn_grn, btn_ylw, btn_blu]
+labels = ["red", "green", "yellow", "blue"]
